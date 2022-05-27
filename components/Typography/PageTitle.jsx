9965c17de44e06a05f59285246c9ddb1cde9321a
Will Blase <william.blase@gmail.com>
2022-05-26 21:51:19 -0700

    Fix build + even more styling

--- components/Typography/PageTitle.jsx
+++ components/Typography/PageTitle.jsx
@@ -1,5 +1,5 @@
 const PageTitle = ({ title }) => {
-	return <div className="text-3xl sm:text-4xl md:text-5xl text-center text-gray-700 mb-4">{title}</div>;
+	return <div className="w-full text-3xl sm:text-4xl md:text-5xl text-center text-gray-700 mb-4">{title}</div>;
 };
 
 export default PageTitle;
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,27 +1,25 @@
 import Header from 'components/Header/CustomHeader';
 import PageTitle from 'components/Typography/PageTitle';
-import React, { useState } from 'react';
 import { useMoralis, useNFTBalances } from 'react-moralis';
 import { NFTBalance } from 'web3uikit';
 
-
-
-function Portfolio() {
+const Portfolio = () => {
 	const { data: NFTBalances } = useNFTBalances();
-	const { Moralis, chainId, user } = useMoralis();
-	const [visible, setVisibility] = useState(false);
+	const { user } = useMoralis();
 
 	console.log('NFTBalances', NFTBalances);
 	console.log('MoralisUser', user);
 	return (
 		<>
 			<Header title="Portfolio | NomadHouse" />
-			<PageTitle title="Portfolio" />
-			<div className="flex-wrap m-auto w-1/1 gap-2.5">
-				<NFTBalance address={user.get('ethAddress')} chain="eth" />
+			<div className="flex-wrap">
+				<PageTitle title="Portfolio" />
+				<div>
+					<NFTBalance className="my-8" address={user?.get('ethAddress')} chain="eth" />
+				</div>
 			</div>
 		</>
 	);
-}
+};
 
 export default Portfolio;
